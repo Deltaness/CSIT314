8a1daa7e32815df0048651f3995df754fa5e33b8
Deltaness <deltaness118@gmail.com>
2022-05-13 20:56:57 +1000

    info

--- commission/commission.js
+++ commission/commission.js
@@ -1,4 +1,3 @@
 function back() {
-    // window.open("../prefessional/professional.html", target = "_self");
-    window.open("../customer/customer.html",target="_self");
+    window.open("../professional/professional.html", target = "_self");
 }
